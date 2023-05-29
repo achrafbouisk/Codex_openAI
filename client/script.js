b675d4dcad7718ca3262f3254d3f52aba2511617
AkramDev <akram.esaaidi@usmba.ac.ma>
2023-05-29 16:06:57 +0100

    "update script.js"

--- client/script.js
+++ client/script.js
@@ -1,19 +1,19 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
 function loader(element) {
-  element.textContent = '';
+  element.textContent = "";
 
   loadInterval = setInterval(() => {
-    element.textContent += '.';
+    element.textContent += ".";
 
-    if (element.textContent === '....') {
-      element.textContent = '';
+    if (element.textContent === "....") {
+      element.textContent = "";
     }
   }, 300);
 }
@@ -41,44 +41,32 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-                                                                                <div class="wrapper ${
-                                                                                  isAi &&
-                                                                                  'ai'
-                                                                                }">
-                                                                                        <div class="chat">
-                                                                                                  <div class="profile">
-                                                                                                              <img 
-                                                                                                                            src="${
-                                                                                                                              isAi
-                                                                                                                                ? bot
-                                                                                                                                : user
-                                                                                                                            }"
-                                                                                                                                          alt="${
-                                                                                                                                            isAi
-                                                                                                                                              ? 'bot'
-                                                                                                                                              : 'user'
-                                                                                                                                          }"
-                                                                                                                                                      />
-                                                                                                                                                                </div>
-                                                                                                                                                                          <div class="message" id=${uniqueId}>${value}</div>
-                                                                                                                                                                                  </div>
-                                                                                                                                                                                        </div>
-                                                                                                                                                                                            `;
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+        </div>
+        <div class="message" id=${uniqueId}>
+          ${value}
+        </div>
+      </div>
+    </div>
+  `;
 }
 
-const handleSubmit = async e => {
+const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
 
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
   form.reset();
 
   // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
@@ -88,18 +76,18 @@ const handleSubmit = async e => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://codex-jza6.onrender.com/', {
-    method: 'POST',
+  const response = await fetch("https://codex-jza6.onrender.com/", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: data.get('prompt'),
+      prompt: data.get("prompt"),
     }),
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
+  messageDiv.innerHTML = "";
 
   if (response.ok) {
     const data = await response.json();
@@ -109,14 +97,14 @@ const handleSubmit = async e => {
   } else {
     const err = await response.text();
 
-    messageDiv.innerHTML = 'Something went wrong';
+    messageDiv.innerHTML = "Something went wrong";
 
     alert(err);
   }
 };
 
-form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup', e => {
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
